5e46f99e27f3f9628642eb9818c083a1f6bffa83
chris <christhirst@gmail.com>
2023-10-17 20:18:18 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,19 @@ use rapi::run;
 
 // While exploring, remove for prod.
 
-
 #[tokio::main]
-async fn main() {
+async fn main() -> surrealdb::Result<()> {
+    let db = Surreal::new::<Ws>("https://surrealdbworld.fly.dev:8080").await?;
 
-    run("").await;
-   
+    // Signin as a namespace, database, or root user
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await?;
+
+    // Select a specific namespace / database
+    db.use_ns("test").use_db("test").await;
+
+    //run("").await;
 }
