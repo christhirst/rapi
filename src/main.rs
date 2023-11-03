ea47bd36060ef2998d5cbaea9944ee19285c5663
chris <christhirst@gmail.com>
2023-11-03 21:22:47 +0100

    server

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 #![allow(unused)]
 
+use std::net::SocketAddr;
+
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
 use rapi::run;
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::remote::ws::{Ws, Wss};
+use surrealdb::opt::auth::Root;
+use surrealdb::sql::Thing;
+use surrealdb::Surreal;
 
 // While exploring, remove for prod.
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-    let db = Surreal::new::<Ws>("https://surrealdbworld.fly.dev:8080").await?;
+    let routers_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World</strong>") }),
+    );
+    let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+    axum::Server::bind(&addr)
+        .serve(routers_hello.into_make_service())
+        .await
+        .unwrap();
 
-    // Signin as a namespace, database, or root user
+    /* let db = Surreal::new::<Wss>("surrealdbworld.fly.dev").await?;
     db.signin(Root {
-        username: "root",
-        password: "root",
+        username: "dbadmin",
+        password: "M0skwa!",
     })
-    .await?;
+    .await?; */
 
-    // Select a specific namespace / database
-    db.use_ns("test").use_db("test").await;
-
+    Ok(())
     //run("").await;
 }
